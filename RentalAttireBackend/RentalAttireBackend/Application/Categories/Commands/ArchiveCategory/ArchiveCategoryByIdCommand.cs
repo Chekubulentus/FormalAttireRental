c112c3727d54ab87219ba7f4f87efb23ec81e5d1
Edelson Barrios <barrios.edelson.quiling@gmail.com>
2026-03-19 08:03:59 +0800

    category archived function backend

---
+++ RentalAttireBackend/RentalAttireBackend/Application/Categories/Commands/ArchiveCategory/ArchiveCategoryByIdCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using RentalAttireBackend.Application.Common.Models;
+
+namespace RentalAttireBackend.Application.Categories.Commands.ArchiveCategory
+{
+    public class ArchiveCategoryByIdQuery : IRequest<Result<bool>>
+    {
+        public int Id { get; set; }
+        public string PerformedBy { get; set; } = string.Empty;
+        public int PerformedById { get; set; }
+    }
+}
---
+++ RentalAttireBackend/RentalAttireBackend/Application/Categories/Commands/ArchiveCategory/ArchiveCategoryByIdCommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using RentalAttireBackend.Application.Common.Models;
+using RentalAttireBackend.Domain.Interfaces;
+
+namespace RentalAttireBackend.Application.Categories.Commands.ArchiveCategory
+{
+    public class ArchiveCategoryByIdCommandHandler : IRequestHandler<ArchiveCategoryByIdCommand, Result<bool>>
+    {
+        private readonly ITransactionManager _transaction;
+        private readonly ICategoryRepository _categoryRepo;
+
+        public ArchiveCategoryByIdCommandHandler(
+            ICategoryRepository categoryRepo,
+            ITransactionManager transaction
+            )
+        {
+            _categoryRepo = categoryRepo;
+            _transaction = transaction;
+        }
+
+        public async Task<Result<bool>> Handle(ArchiveCategoryByIdCommand request, CancellationToken cancellationToken)
+        {
+            if (request is null)
+                return Result<bool>.Failure("Invalid request.");
+
+            if (request.PerformedBy is null || request.PerformedById == 0)
+                return Result<bool>.Failure("Employee responsible for this transaction could not be identified.");
+            
+            try
+            {
+                await _transaction.BeginTransactionAsync(cancellationToken);
+
+                var category = await _categoryRepo.GetCategoryByIdAsync(request.Id, cancellationToken);
+
+                if(category is null)
+                {
+                    await _transaction.RollbackTransactionAsync(cancellationToken);
+                    return Result<bool>.Failure("Category does not exist.");
+                }
+
+                category.IsActive = false;
+
+                var updateCategory = await _categoryRepo.UpdateCategoryAsync(category, cancellationToken);
+
+                if(!updateCategory)
+                {
+                    await _transaction.RollbackTransactionAsync(cancellationToken);
+                    return Result<bool>.Failure("Category could not be archived.");
+                }
+
+                await _transaction.CommitTransacionAsync(cancellationToken);
+                return Result<bool>.SuccessWithMessage("Category successfully archived.");
+            }catch(Exception e)
+            {
+                await _transaction.RollbackTransactionAsync(cancellationToken);
+                return Result<bool>.Failure(e.Message);
+            }
+        }
+    }
+}
